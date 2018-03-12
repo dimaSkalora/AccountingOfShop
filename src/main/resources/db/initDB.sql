4ff7553887760caa2b5c789411df3baa8196e455
dimaSkalora <timon2@ukr.net>
2018-03-12 10:00:22 +0200

    resources - db

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,10 +1,32 @@
+DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS alcohol;
+DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
-CREATE SEQUENCE global_seq START 1;
+CREATE SEQUENCE global_seq START 100000;
+
+CREATE TABLE users
+(
+  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name             VARCHAR                 NOT NULL,
+  email            VARCHAR                 NOT NULL,
+  password         VARCHAR                 NOT NULL,
+  registered       TIMESTAMP DEFAULT now() NOT NULL,
+  enabled          BOOL DEFAULT TRUE       NOT NULL
+);
+CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+
+CREATE TABLE user_roles
+(
+  user_id INTEGER NOT NULL,
+  role    VARCHAR,
+  CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
 
 CREATE TABLE alcohol (
-  id   INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id INTEGER NOT NULL,
   goodsReceiptDate DATE NOT NULL,
   category VARCHAR NOT NULL,
   productName VARCHAR NOT NULL,
@@ -12,5 +34,8 @@ CREATE TABLE alcohol (
   balanceOnTheFirstDayOfTheMonth INTEGER NOT NULL,
   receivedForMonth INTEGER NOT NULL,
   soldForMonth INTEGER NOT NULL,
-  balanceOnTheLastDayOfTheMonth INTEGER NOT NULL
+  balanceOnTheLastDayOfTheMonth INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+CREATE UNIQUE INDEX alcohol_unique_user_alcoholid_idx ON alcohol (id, user_id)
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,24 @@
+DELETE FROM user_roles;
 DELETE FROM alcohol;
+DELETE FROM users;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-ALTER SEQUENCE global_seq RESTART WITH 1;
+INSERT INTO users (name, email, password) VALUES
+  ('User', 'user@yandex.ru', '{noop}password'),
+  ('Admin', 'admin@gmail.com', '{noop}admin');
 
-INSERT INTO alcohol (goodsReceiptDate, category, productName, liter, balanceOnTheFirstDayOfTheMonth, receivedForMonth, soldForMonth, balanceOnTheLastDayOfTheMonth) VALUES
-('2018-03-11','вино', 'productName1', 1.5, 3, 5, 4, 1),
-('2018-03-11','вино', 'productName2', 0.5, 3, 5, 4, 1),
-('2018-03-11','вино', 'productName3', 0.7, 3, 5, 4, 1);
+INSERT INTO user_roles (role, user_id) VALUES
+  ('ROLE_USER', 100000),
+  ('ROLE_ADMIN', 100001),
+  ('ROLE_USER', 100001);
+
+INSERT INTO alcohol (goodsReceiptDate, category, productName, liter, balanceOnTheFirstDayOfTheMonth, receivedForMonth, soldForMonth, balanceOnTheLastDayOfTheMonth, user_id) VALUES
+('2018-03-11','вино', 'productName1', 1.5, 3, 5, 4, 1, 100001),
+('2018-03-11','вино', 'productName2', 0.5, 3, 5, 4, 1, 100001),
+('2018-03-11','вино', 'productName3', 0.7, 3, 5, 4, 1, 100001),
+('2018-03-11','вино', 'productName4', 1.5, 3, 5, 4, 1, 100000),
+('2018-03-11','вино', 'productName5', 0.5, 3, 5, 4, 1, 100000),
+('2018-03-11','вино', 'productName6', 0.7, 3, 5, 4, 1, 100000);
 
 
 
