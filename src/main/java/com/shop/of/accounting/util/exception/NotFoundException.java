068bb68c296757455d2c59260e4075645c470624
dimaSkalora <timon2@ukr.net>
2018-03-13 11:30:25 +0200

    util - validation

---
+++ src/main/java/com/shop/of/accounting/util/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.shop.of.accounting.util.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
