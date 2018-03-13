568b73ad5c9606bbdace4e446432d099c373430f
dimaSkalora <timon2@ukr.net>
2018-03-13 17:09:22 +0200

    test.service - userServiceTest

---
+++ src/test/java/com/shop/of/accounting/service/AbstractUserServiceTest.java
@@ -0,0 +1,85 @@
+package com.shop.of.accounting.service;
+
+import com.shop.of.accounting.model.Role;
+import com.shop.of.accounting.model.User;
+import com.shop.of.accounting.util.exception.NotFoundException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import static com.shop.of.accounting.UserTestData.*;
+
+
+public class AbstractUserServiceTest extends AbstractServiceTest{
+
+    @Autowired
+    protected UserService service;
+
+    @Test
+    public void create() throws Exception {
+        User newUser = new User(null, "New", "new@gmail.com", "newPass",  false,new Date(), Collections.singleton(Role.ROLE_USER));
+        User created = service.create(newUser);
+        newUser.setId(created.getId());
+        assertMatch(service.getAll(), ADMIN, newUser, USER);
+    }
+
+    @Test(expected = DataAccessException.class)
+    public void duplicatPetCreate() throws Exception {
+        service.create(new User(null, "Duplicate", "user@yandex.ru", "newPass", Role.ROLE_USER));
+    }
+
+    @Test
+    public void delete() throws Exception {
+        service.delete(USER_ID);
+        assertMatch(service.getAll(), ADMIN);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void notFoundDelete() throws Exception {
+        service.delete(1);
+    }
+
+    @Test
+    public void get() throws Exception {
+        User user = service.get(ADMIN_ID);
+        assertMatch(user, ADMIN);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getNotFound() throws Exception {
+        service.get(1);
+    }
+
+    @Test
+    public void getByEmail() throws Exception {
+        User user = service.getByEmail("admin@gmail.com");
+        assertMatch(user, ADMIN);
+    }
+
+    @Test
+    public void update() throws Exception {
+        User updated = new User(USER);
+        updated.setName("UpdatedName");
+        updated.setRoles(Collections.singletonList(Role.ROLE_ADMIN));
+        service.update(updated);
+        assertMatch(service.get(USER_ID), updated);
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        List<User> all = service.getAll();
+        assertMatch(all, ADMIN, USER);
+    }
+
+    @Test
+    public void testEnable() {
+        service.enable(USER_ID, false);
+        Assert.assertFalse(service.get(USER_ID).isEnabled());
+        service.enable(USER_ID, true);
+        Assert.assertTrue(service.get(USER_ID).isEnabled());
+    }
+}
