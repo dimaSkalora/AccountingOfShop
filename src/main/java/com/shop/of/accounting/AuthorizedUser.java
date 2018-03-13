327d167d13df7b199b60f9652f7ac6041c4bd345
dimaSkalora <timon2@ukr.net>
2018-03-13 17:50:55 +0200

    AuthorizedUser

---
+++ src/main/java/com/shop/of/accounting/AuthorizedUser.java
@@ -0,0 +1,16 @@
+package com.shop.of.accounting;
+
+import com.shop.of.accounting.model.AbstractBaseEntity;
+
+public class AuthorizedUser {
+    private static int id = AbstractBaseEntity.START_SEQ;
+
+    public static int id() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        AuthorizedUser.id = id;
+    }
+
+}
