4e9b17aeb8fe1b8dfb55b18381e3f8600fca983c
dimaSkalora <timon2@ukr.net>
2018-03-21 19:38:53 +0200

    JSON

--- src/main/java/com/shop/of/accounting/model/AbstractBaseEntity.java
+++ src/main/java/com/shop/of/accounting/model/AbstractBaseEntity.java
@@ -1,7 +1,12 @@
 package com.shop.of.accounting.model;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.shop.of.accounting.HasId;
 
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
+
+//@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
 public abstract class AbstractBaseEntity implements HasId {
     public static final int START_SEQ = 100000;
 
