ec2b0a30904b7246b65fa15f1b48449517781f30
dimaSkalora <timon2@ukr.net>
2018-03-30 12:34:42 +0300

    web - RootController fix

--- src/main/java/com/shop/of/accounting/web/RootController.java
+++ src/main/java/com/shop/of/accounting/web/RootController.java
@@ -1,30 +1,29 @@
 package com.shop.of.accounting.web;
 
 import com.shop.of.accounting.AuthorizedUser;
-import com.shop.of.accounting.model.Role;
-import com.shop.of.accounting.model.User;
-import com.shop.of.accounting.service.UserService;
-import com.shop.of.accounting.web.user.AbstractUserController;
-import com.shop.of.accounting.web.user.JspUserController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.awt.print.Book;
-import java.util.HashSet;
-import java.util.Set;
 
 @Controller
 public class RootController {
 
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+
     @GetMapping("/")
-    public String root(){
-        return "/index";
+    public String root()
+    {
+        return "goods";
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
     }
 
     @PostMapping("/setUser")
