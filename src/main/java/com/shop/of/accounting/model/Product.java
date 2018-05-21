31453bcc7940fd18df808017178885fd902c5085
dimaSkalora <timon2@ukr.net>
2018-05-21 19:45:54 +0300

    model - Product add annotations JPA

--- src/main/java/com/shop/of/accounting/model/Product.java
+++ src/main/java/com/shop/of/accounting/model/Product.java
@@ -1,17 +1,37 @@
 package com.shop.of.accounting.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "product", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id"}, name = "alcohols_unique_user_datetime_idx")})
 public class Product extends AbstractBaseEntity{
+    @Column(name = "goodsReceiptDate", nullable = false)
     private LocalDate goodsReceiptDate;
+
+    @Column(name = "category", nullable = false)
     private String category;
+
+    @Column(name = "productName", nullable = false)
     private String productName;
+
+    @Column(name = "amount", nullable = false)
     private Integer amount;
+
+    @Column(name = "balanceOnTheFirstDayOfTheMonth", nullable = false)
     private Integer balanceOnTheFirstDayOfTheMonth;
+
+    @Column(name = "receivedForMonth", nullable = false)
     private Integer receivedForMonth;
+
+    @Column(name = "soldForMonth", nullable = false)
     private Integer soldForMonth;
+
+    @Column(name = "balanceOnTheLastDayOfTheMonth", nullable = false)
     private Integer balanceOnTheLastDayOfTheMonth;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     public Product() {
