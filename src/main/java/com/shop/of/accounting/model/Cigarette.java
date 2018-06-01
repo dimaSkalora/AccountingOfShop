f27d95434ad2ae96ab9e6ea0cbf10e5b83a8a9ff
dimaSkalora <timon2@ukr.net>
2018-06-01 13:25:17 +0300

    model - Cigarette add @NamedQueries, @NamedQuery

--- src/main/java/com/shop/of/accounting/model/Cigarette.java
+++ src/main/java/com/shop/of/accounting/model/Cigarette.java
@@ -3,9 +3,28 @@ package com.shop.of.accounting.model;
 import javax.persistence.*;
 import java.time.LocalDate;
 
+@NamedQueries({
+    @NamedQuery(name = Cigarette.ALL_SORTED, query = "SELECT c FROM Cigarette c WHERE c.user.id=:userId order by c.goodsReceiptDate DESC "),
+    @NamedQuery(name = Cigarette.DELETE, query = "DELETE FROM Cigarette c " +
+            "WHERE c.id=:id AND c.user.id=:userId"),
+    @NamedQuery(name = Cigarette.GET_CATEGORY, query = "SELECT c FROM Cigarette c " +
+            "WHERE c.category=:category AND c.user.id=:userID"),
+    @NamedQuery(name = Cigarette.GET_SEARCH_BY_PRODUCT_NAME, query = "SELECT c FROM Cigarette c WHERE c.catgeory=:category" +
+            "AND c.productname=:productName  AND c.category=:category AND c.user.id=:userId ORDER BY c.goodsReceiptDate DESC"),
+    @NamedQuery(name = Cigarette.GET_BETWEEN, query = "SELECT c FROM Cigarette c" +
+            "WHERE  c.user.id=:userId  AND c.goodsReceiptDate BETWEEN :startDate AND :endDate ORDER BY c.goodsReceiptDate")
+        
+})
 @Entity
-@Table(name = "cigarette", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id"}, name = "alcohols_unique_user_datetime_idx")})
+@Table(name = "cigarette", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id"}, name = "cigarettes_unique_user_datetime_idx")})
 public class Cigarette extends AbstractBaseEntity{
+
+    public static final String ALL_SORTED = "Cigarette.getAll";
+    public static final String DELETE = "Cigarette.delete";
+    public static final String GET_CATEGORY = "Cigarette.getCategory";
+    public static final String GET_SEARCH_BY_PRODUCT_NAME = "Cigarette.getSearchByProductName(";
+    public static final String GET_BETWEEN = "Cigarette.getBetween";
+
     @Column(name = "goodsReceiptDate", nullable = false)
     private LocalDate goodsReceiptDate;
 
